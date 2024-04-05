de1ff75b1f62815f6c1d6e4234eee9eb80561dde
mshaloy <129133916+mshaloy@users.noreply.github.com>
2024-04-05 13:15:47 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+#---------------------
+#Main
+#Created by Mallory Shaloy 3/2024
+#CCofCO RockSat-X 2024
+#---------------------
+import Activation
+import time
+
+regolith = Activation()
+regolith.event_activation(26, 5, 180)
