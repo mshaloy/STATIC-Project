4b89651566a7998e8f60c7e7eadde31d785c00e3
mshaloy <129133916+mshaloy@users.noreply.github.com>
2024-04-23 16:29:20 -0600

    Update and rename TMP36Sensor.py to tmp36Sensor.py

--- Sensor/tmp36Sensor.py
+++ Sensor/tmp36Sensor.py
@@ -4,17 +4,15 @@
 #CCofCO RockSat-X 2024
 #---------------------
 
-import Sensor
-import board
-import busio
-import adafruit_ads1x15.ads1115 as ADS
+import sensor
 from adafruit_ads1x15.analog_in import AnalogIn
 
-class TMP36Sensor(Sensor):
+class TMP36Sensor(sensor):
     """Class for the TMP36 temperature sensor, supported by the Adafruit ADS1115 analog to digital converter breakout board."""
-    def __init__(self, name, units="Imperial", sleep_t=1.0, pin_number=0, repeat=0):
+    def __init__(self, name, board, units="Imperial", sleep_t=1.0, pin_number=0, repeat=0):
         """Create and initialize a TMP36 sensor object. 
         param: string name: The sensor name.
+        param: ADS1115 board: The ADS1115 board interface the sensor is connected to.
         param: string units: The units of sensor outputs. Default is Imperial.
                             Valid options:
                             Imperial: Fahrenheit
@@ -28,19 +26,15 @@ class TMP36Sensor(Sensor):
         """
         #unit options: "Imperial" or "Metric"
         if units != "Imperial" and units != "Metric":
-            raise Exception("Invalid units for this sensor")
-        else:
-            pass
-        super.__init__(self, name, units, sleep_t, repeat)
-        if pin_number >= 0 and pin_number < 4:
-            self.pin_number = pin_number  # 0 to 3 are valid. Value of pin where sensor is connected
-        else:
+            raise Exception("Invalid units for this sensor.")
+        elif pin_number < 0 or pin_number > 4:
             raise Exception("Invalid pin number.")
+        else:
+            super.__init__(self, name, units, sleep_t, repeat)
+            self.pin_number = pin_number  # 0 to 3 are valid. Value of pin where sensor is connected
 
         #setup Adafruit ADS1115 analog to digital converter board with TMP36 sensor
-        i2c = busio.I2C(board.SCL, board.SDA)
-        ads = ADS.ADS1115(i2c)
-        self.tmp = AnalogIn(ads, self.pin_number) 
+        self.tmp = AnalogIn(board.ads, self.pin_number) 
 
     def takeReading(self):
         reading_data = []
@@ -52,3 +46,4 @@ class TMP36Sensor(Sensor):
         else:
             reading_data = [temp_in_C]
             return reading_data
+            
