059eda84f871c1879a2469c504d8257d0f4175ef
STATIC <staticccofco@outlook.com>
2024-05-07 13:49:14 -0600

    add additional process for experiment actions

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 #Created by Mallory Shaloy 3/2024
 #CCofCO RockSat-X 2024
 #---------------------
-from sensor import Sensor
+import sensor
 from activation import Activation
 from bme280Sensor import BME280Sensor
 from tmp36Sensor import TMP36Sensor
@@ -26,47 +26,50 @@ def runSensor_console(sensor):
     _sensor = sensor
     _sensor.takeReading()
     _sensor.printToConsole()
-
-if __name__ == "__main__":
-    try:
-        i2c = busio.I2C(board.SCL, board.SDA)
-        ads = ADS.ADS1115(i2c)              # analog to digital converter board
-        bme = BME280Sensor("BME", "Metric", 0.1)        # Set up BME280 sensor; Will take readings continuously every 0.1 seconds
-        tmp = TMP36Sensor("TMP", ads, "Metric", 0.1, 0)      # Set up TMP36 sensor; Will take readings continuously every 0.1 seconds
-
-        t1 = multiprocessing.Process(target=runSensor_file(bme))    #assigning running bme280 sensor to process 1 (number for id purposes only)
-        t2 = multiprocessing.Process(target=runSensor_file(tmp))    #assigning running tmp36 sensor to proccess 2 (number for id purposes only)
-
-        #starting individual proccesses
-        t1.start()
-        t2.start()
-
-        #begin regolith sequence
+    
+def runExperiment():
+    #begin regolith sequence
         regolith = Activation()
-        regolith.eventActivation(26, 5, 180)        #listening for TE-2 on GPIO 26; activating GPIO 5 for 180 seconds
+        regolith.eventActivation(5, 26, 180)        #listening for TE-2 on GPIO 5; activating GPIO 26 for 180 seconds
 
         #initiate star tracker sequence
-        star_tracker = StarTracker()
-        angle = -30.0       #initial angle is -30 degrees
+        star_tracker = StarTracker(25)
+        angle = -120.0       #initial angle is -120 degrees to account for angle adjustment interaction with looping
         image_number = 0    #inital image number
-        star_tracker.moveCamera(angle)
 
-        #take 4 images at each of 3 star tracker locations; repeat 3 times
+        #take 3 images at each of 3 star tracker locations; repeat 3 times
         for i in range(3):
+            angle = -120
             for i in range(3):
-                for i in range(4):
-                    star_tracker.takePicture(image_number)
-                    image_number += 1
-                angle += 30
+                angle += 60		#adjust camera angle through 3 locations -60, 0, and 60 degrees
                 star_tracker.moveCamera(angle)
-            angle = -30
-            star_tracker.moveCamera(angle)
-        
-        #LOST software calculations
-        star_tracker.trackerCalculation()
+                for i in range(3):
+                    star_tracker.takePicture(image_number)
+                    image_number += 1		#increment image number on captured images
+            
 
+        #LOST software calculations
+        star_tracker.trackerCalculation(image_number)
+        
+if __name__ == "__main__":
+    try:
+        i2c = busio.I2C(board.SCL, board.SDA)	#setup i2c interface
+        ads = ADS.ADS1115(i2c)              # setup analog to digital converter board
+        bme = BME280Sensor("BME", "Metric", 0.1, 10)        # Set up BME280 sensor; Will take readings continuously every 0.1 seconds
+        tmp = TMP36Sensor("TMP", ads, "Metric", 0.1, 1, 10)      # Set up TMP36 sensor; Will take readings continuously every 0.1 seconds
+        t1 = multiprocessing.Process(target=runSensor_file(bme))    #assigning running bme280 sensor to process 1 (number for id purposes only)
+        t2 = multiprocessing.Process(target=runSensor_file(tmp))    #assigning running tmp36 sensor to process 2 (number for id purposes only)
+        t3 = multiprocessing.Process(target=runExperiment())		#assigning running experiment functions to process 3 (number for id purposes only)
+
+        #starting individual proccesses. One sensor per process plus experiment sequence on one proccess.
+        t1.start()
+        t2.start()
+        t3.start()
+
+        t3.join()
         t1.join()
         t2.join()
 
-    except:
-        pass
+    except Exception as e:
+        print(e)
+ 
